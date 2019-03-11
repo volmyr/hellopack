0e35c46b2f421a6b0dcf4ba41c3b643b10de9d56
volmyr <vol.kulyk@gmail.com>
2019-03-11 16:43:33 +0200

    v0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="example-pkg-your-username",
-    version="0.0.1",
+    version="0.0.3",
     author="Example Author",
     author_email="author@example.com",
     description="A small example package",
